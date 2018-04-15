9e0ff6796e8b40d7d2d68a524d00b57a6a17bbd2
tshemsedinov <timur.shemsedinov@gmail.com>
2018-04-16 00:08:13 +0300

    Reorder examples

--- JavaScript/2-await-sleep.js
+++ JavaScript/2-await-sleep.js
@@ -2,11 +2,9 @@
 
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
-const test = async () => {
+(async () => {
   console.log('Start sleep: ' + new Date().toISOString());
   console.log('  Sleep about 3 sec');
   await sleep(3000);
   console.log('After sleep: ' + new Date().toISOString());
-};
-
-test();
+})();
